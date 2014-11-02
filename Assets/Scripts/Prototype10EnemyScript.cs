1703df4acec4789154064fd506191e61f9d1b48d
jm404 <jhm@gmail.com>
2014-11-02 00:20:10 -0300

    Added health bars for enemies
    Enemies have health bars, reduced the size of the rat

--- Assets/Scripts/Prototype10EnemyScript.cs
+++ Assets/Scripts/Prototype10EnemyScript.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class Prototype10EnemyScript : MonoBehaviour 
 {
 	public GameObject attackScreen;//Screen to display when the enemy attacks the player (flashes a red screen)
+	public Texture2D healthBarFull;
+	public Texture2D healthBarEmpty;
 	public float speed = 5.0f;//Speed enemy moves at
 	public float attackTime = 5.0f;
 	public int damage = 10;
@@ -16,6 +18,7 @@ public class Prototype10EnemyScript : MonoBehaviour
 	private Vector2 movement;//Enemy's movement
 
 	public float health = 100;//Enemy's health
+	private float fullHealth;
 
 	private bool onFire = false;//determine if enemy is on fire
 	private bool isAttacking = false;//determine if the enemy is attacking the player
@@ -29,6 +32,7 @@ public class Prototype10EnemyScript : MonoBehaviour
 	{
 		rigidbody2D.isKinematic = true;
 		enemyYPostion = transform.position.y;
+		fullHealth = health;
 	}
 
 	public int getHealth()
@@ -137,18 +141,31 @@ public class Prototype10EnemyScript : MonoBehaviour
 	void OnGUI()
 	{
 		Vector2 enemyScreenLocation = Camera.main.WorldToScreenPoint (transform.position);
-		int healthWidth = (int)(Screen.width * 0.3);
-		int healthHeight = (int)(Screen.height * 0.08);
-		
-		GUI.skin.label.fontSize = Screen.width / 15;
-		
-		Rect healthRect = new Rect (
-			enemyScreenLocation.x,
-			Screen.height - (enemyScreenLocation.y + (renderer.bounds.extents.y*250)),
-			healthWidth,
-			healthHeight);
-		
-		GUI.Label (healthRect, ""+getHealth());
+//		int healthWidth = (int)(Screen.width * 0.3);
+//		int healthHeight = (int)(Screen.height * 0.08);
+//		
+//		GUI.skin.label.fontSize = Screen.width / 15;
+//		
+//		Rect healthRect = new Rect (
+//			enemyScreenLocation.x,
+//			Screen.height - (enemyScreenLocation.y + (renderer.bounds.extents.y*250)),
+//			healthWidth,
+//			healthHeight);
+//		
+//		GUI.Label (healthRect, ""+getHealth());
+//
+//		Vector3 pos = Camera.main.WorldToScreenPoint (transform.position);
+		Vector3 size = new Vector3 (300, 50, 0);
+		// draw the background:
+		GUI.BeginGroup (new Rect (enemyScreenLocation.x, Screen.height - (enemyScreenLocation.y + (renderer.bounds.extents.y*250)), size.x, size.y));
+		GUI.DrawTexture (new Rect (0,0, size.x, size.y),healthBarEmpty);
+
+			// draw the filled-in part:
+			GUI.BeginGroup (new Rect (0, 0, size.x * health/fullHealth, size.y));
+				GUI.DrawTexture (new Rect (0,0, size.x, size.y),healthBarFull);
+			GUI.EndGroup ();
+					
+		GUI.EndGroup ();
 	}
 
 	void OnTriggerEnter2D(Collider2D other)
