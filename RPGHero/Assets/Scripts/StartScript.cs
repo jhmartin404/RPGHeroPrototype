88a50c67d16922f7fed3448b5955668fc72542e4
jm404 <jhm@gmail.com>
2015-01-11 01:00:10 -0400

    Added Start Scene and font
    Start scene now added.  Added in game font

---
+++ RPGHero/Assets/Scripts/StartScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScript : MonoBehaviour 
+{
+	private float fingerRadius = 0.5f;
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if(Application.platform == RuntimePlatform.Android)
+		{
+			if(Input.GetKey(KeyCode.Escape))
+			{
+				Application.Quit();
+			}
+		}
+	}
+
+	public void ChangeScene()
+	{
+		Application.LoadLevel("LevelSelectScene");
+	}
+}
