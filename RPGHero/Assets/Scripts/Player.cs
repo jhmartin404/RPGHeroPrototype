1c1502b5812226924f5898e54810a6b93a1230e8
jm404 <jhm@gmail.com>
2015-01-21 01:02:14 -0400

    Added a Json Serializer/Deserializer, Started Load and Save
    Started working on Load and Save for Player (testing out
    Serializer/Deserializer)

--- RPGHero/Assets/Scripts/Player.cs
+++ RPGHero/Assets/Scripts/Player.cs
@@ -1,12 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using MiniJSON;
 
 public class Player 
 {
 	private static Player instance;
 
 	private float health;
-	private float coins;
+	private int coins;
 	private float stamina;
 	private float mana;
 	private PlayerStats playerStats;
@@ -14,8 +16,7 @@ public class Player
 
 	private Player() 
 	{
-		playerStats = new PlayerStats ();
-		playerInventory = new Inventory ();
+		Load ();
 	}
 
 	public static Player Instance
@@ -37,12 +38,63 @@ public class Player
 
 	public PlayerStats getPlayerStats()
 	{
+		Save ();
 		return playerStats;
 	}
 
 	public Inventory getPlayerInventory()
 	{
 		return playerInventory;
+	}
+
+	void Load()
+	{
+		//PlayerPrefs.DeleteAll ();
+		if(PlayerPrefs.HasKey("Player"))
+		{
+			Debug.Log("Player Here");
+
+			string player = PlayerPrefs.GetString("Player");
+			Debug.Log("Load: " + player);
+
+			Dictionary<string, object> dict = new Dictionary<string, object>();
+			dict = Json.Deserialize(player) as Dictionary<string, object>;
+
+			object obj = dict["health"];
+			health = (float)(double)obj;
+
+			obj = dict["coins"];
+			coins = (int)(double)obj;
+
+			obj = dict["stamina"];
+			stamina = (float)(double)obj;
+
+			obj = dict["mana"];
+			mana = (float)(double)obj;
+
+			playerStats = new PlayerStats ();
+			playerInventory = new Inventory ();
+		}
+		else
+		{
+			Debug.Log("Player not there");
+			playerStats = new PlayerStats ();
+			playerInventory = new Inventory ();
+		}
+	}
+
+	public void Save()
+	{
+		Dictionary<string, object> dictionary = new Dictionary<string, object>();
+
+		dictionary.Add ("health", health+2.01);
+		dictionary.Add ("coins", coins+2.01);
+		dictionary.Add ("stamina", stamina+2.01);
+		dictionary.Add ("mana", mana+2.01);
+
+		string saveJSON = Json.Serialize (dictionary);
+		Debug.Log ("Save: " + saveJSON);
+		PlayerPrefs.SetString ("Player", saveJSON);
 	}
 
 	// Use this for initialization
